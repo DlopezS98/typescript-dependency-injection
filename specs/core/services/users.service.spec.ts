6e4c6014ac8920047e352f4b8e3f3321bb72c79d
DlopezS98 <01dlopezs98@gmail.com>
2022-03-25 16:49:37 -0600

    first unit test for users service

--- specs/core/services/users.service.spec.ts
+++ specs/core/services/users.service.spec.ts
@@ -1,5 +1,62 @@
-// import UsersService from '@Services/users.service';
+import 'reflect-metadata';
+import UsersService from '@Services/users.service';
+import IUsersRepository from '@Interfaces/repositories/iusers.repository';
+import Users from '@Database/models/users.model';
+import UserDto from '@Shared/Dtos/responses/users.dto';
+import usersModelMock from '../../mocks/users.mock';
 
-it('should return a list of users', () => {
-  expect(true).toEqual(true);
+let usersService: UsersService;
+let userRepository: IUsersRepository;
+let users: UserDto[];
+
+beforeAll(() => {
+  const Mock = jest.fn<IUsersRepository, Users[]>(() => ({
+    getAll: jest.fn().mockReturnValue(usersModelMock),
+    getById: jest
+      .fn()
+      .mockImplementation((id: string) =>
+        usersModelMock.find((user) => user.id === id)
+      ),
+  }));
+
+  userRepository = new Mock();
+  usersService = new UsersService(userRepository);
+});
+
+describe('(method): getAll - should return a list of users', () => {
+  beforeAll(() => {
+    users = usersService.getAll();
+  });
+
+  it('should have more tha one record', () => {
+    expect(users.length).toBeGreaterThan(0);
+  });
+
+  it('users should be an array of user dtos', () => {
+    const firstUser = usersModelMock[0];
+    const fullname = `${firstUser.firstname} ${firstUser.lastname}`;
+    const mockedUser: UserDto = {
+      id: firstUser.id,
+      username: firstUser.username,
+      createdAt: firstUser.created_at,
+      fullname,
+      updatedAt: firstUser.updated_at,
+    };
+
+    expect(users).toContainEqual<UserDto>(mockedUser);
+    users.forEach((user) => {
+      expect(user).toHaveProperty<UserDto>('fullname');
+      expect(user).not.toHaveProperty<UserDto>('firstname');
+      expect(user).not.toHaveProperty<UserDto>('lastname');
+    });
+  });
+});
+
+describe('(method): getById - should return a single user', () => {
+  it('return a single user', () => {
+    const user1 = usersService.getById('1200012');
+    const user2 = usersService.getById('1288120');
+    expect(user1?.username).toEqual('DlopezS98');
+    expect(user2?.username).toEqual('01DlopezS98');
+  });
 });
---
+++ specs/mocks/users.mock.ts
@@ -0,0 +1,22 @@
+import Users from '@Database/models/users.model';
+
+const usersModel: Users[] = [
+  {
+    id: '1200012',
+    firstname: 'Danny',
+    lastname: 'Lopez',
+    username: 'DlopezS98',
+    password: '***Gen3ricP@ssword***',
+    created_at: new Date('2022-03-25T21:25:00.227Z'),
+  },
+  {
+    id: '1288120',
+    firstname: 'Aldahir',
+    lastname: 'Sanchez',
+    username: '01DlopezS98',
+    password: '*_*Gen3ricP@ssword*_*',
+    created_at: new Date('2022-03-25T21:25:00.227Z'),
+  },
+];
+
+export default usersModel;
