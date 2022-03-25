598d49d1fbd0507dcbcc0bdac11d62834072c645
DlopezS98 <01dlopezs98@gmail.com>
2022-03-25 16:48:37 -0600

    setting up relative paths for jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "^@Database/(.*)$": "<rootDir>/src/core/database/$1",
+    "^@Controllers/(.*)$": "<rootDir>/src/controllers/$1",
+    "^@Repositories/(.*)$": "<rootDir>/src/core/repositories/$1",
+    "^@Services/(.*)$": "<rootDir>/src/core/services/$1",
+    "^@Interfaces/(.*)$": "<rootDir>/src/core/interfaces/$1",
+    "^@Routes/(.*)$": "<rootDir>/src/routes/$1",
+    "^@Shared/(.*)$": "<rootDir>/src/shared/$1",
+    "^@Config/(.*)$": "<rootDir>/src/config/$1"
+  }
 };
